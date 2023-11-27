a07849bcfed6ce6c1eeaf6c7ff07354af1b25984
João Paulo <fernandes.joao@estudante.ufjf.br>
2023-11-27 16:21:37 -0300

    Altera valores de joao e maria

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+import { getJoao, getMaria, deJoaoParaMaria } from "./joaoEMaria.js";
+
 console.log('módulo main');
+
+console.log('João tem ', getJoao(), ' maçãs!');
+console.log('Maira tem ', getMaria(), ' maçãs!');
+
+deJoaoParaMaria();
+
+console.log('João tem ', getJoao(), ' maçãs!');
+console.log('Maira tem ', getMaria(), ' maçãs!');
